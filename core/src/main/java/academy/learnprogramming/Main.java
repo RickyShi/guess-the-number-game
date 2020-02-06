eaea2589d5f5bc1e5fcdf68d1dd7b9bf30c2b583
Ricky <rickyshi0823@gmail.com>
2020-02-05 17:57:47 -0700

    Beans as Components

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -15,7 +15,7 @@ public class Main {
         //create context(container)
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
         //get numberGenerator bean from context (container)
-        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
 
         //call method next() to get a random number
         int number = numberGenerator.next();
